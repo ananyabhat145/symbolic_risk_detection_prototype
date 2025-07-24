079096afec6c02268e4ccb7b0dbcccaaf866ab7c
ananyabhat145 <161270886+ananyabhat145@users.noreply.github.com>
2025-07-24 15:10:54 -0400

    Update scenarios.py

--- scenarios.py
+++ scenarios.py
@@ -1,1 +1,11 @@
-
+def sample_network():
+    nodes = [
+        {"id": "A"}, {"id": "B"}, {"id": "C"}, {"id": "D"}
+    ]
+    edges = [
+        {"source": "A", "target": "B", "weight": 2.0},
+        {"source": "B", "target": "C", "weight": 1.5},
+        {"source": "C", "target": "D", "weight": 2.2},
+        {"source": "D", "target": "A", "weight": 1.0}
+    ]
+    return {"nodes": nodes, "edges": edges}
